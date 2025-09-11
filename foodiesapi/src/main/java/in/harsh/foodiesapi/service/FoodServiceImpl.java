abe4b530540e3c52ddd6ded01dd211ae01e2545c
Harshbhati30 <bhatiharsh9818@gmail.com>
2025-09-11 14:47:56 +0530

    S3Client Service

--- foodiesapi/src/main/java/in/harsh/foodiesapi/service/FoodServiceImpl.java
+++ foodiesapi/src/main/java/in/harsh/foodiesapi/service/FoodServiceImpl.java
@@ -5,22 +5,60 @@ import in.harsh.foodiesapi.io.FoodRequest;
 import in.harsh.foodiesapi.io.FoodResponse;
 import in.harsh.foodiesapi.repository.FoodRepository;
 import lombok.AllArgsConstructor;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectResponse;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Service
+//@AllArgsConstructor
 public class FoodServiceImpl implements FoodService {
+
 
     @Autowired
     private FoodRepository foodRepository;
 
+    @Autowired
+    private S3Client s3Client;
+
+    @Value("${aws.s3.bucketname}")
+    private String bucketName;
+
     @Override
     public String uploadFile(MultipartFile file) {
-        return "";
+        String filenameExtention= file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf(".")+1);
+        String Key = UUID.randomUUID().toString() + "." + filenameExtention;
+        try{
+            PutObjectRequest putObjectRequest=PutObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(Key)
+                    .acl("public-read")
+                    .contentType(file.getContentType())
+                    .build();
+
+            PutObjectResponse response=s3Client.putObject(putObjectRequest, RequestBody.fromBytes(file.getBytes()));
+
+            if(response.sdkHttpResponse().isSuccessful()){
+                return "https://" + bucketName + ".s3.amazonaws.com/" + Key;
+            }else{
+                throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,"File upload failed");
+            }
+        }catch (IOException ex){
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,"An Error Occured while uploading the file");
+
+        }
     }
 
     @Override
